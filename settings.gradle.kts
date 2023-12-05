c045156a7710cf4ddb2f119a189e0d80d1887dd4
froilan <froilanaquino1@gmail.com>
2023-12-06 01:54:48 +0800

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Movies_Ticketing_Aquino_Villaester_EDP"
+include(":app")
