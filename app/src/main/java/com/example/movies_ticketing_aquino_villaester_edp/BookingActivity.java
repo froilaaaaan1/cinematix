5614d1c34f3ae751620dbf5ed452f43ac36a26d5
froilan <froilanaquino1@gmail.com>
2023-12-07 02:35:13 +0800

    save point #4

--- app/src/main/java/com/example/movies_ticketing_aquino_villaester_edp/BookingActivity.java
+++ app/src/main/java/com/example/movies_ticketing_aquino_villaester_edp/BookingActivity.java
@@ -10,5 +10,7 @@ public class BookingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_booking);
+
+        // dito yung form fill up
     }
 }
