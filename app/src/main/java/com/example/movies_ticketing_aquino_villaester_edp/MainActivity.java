ec5c663046b791c9d3b0da1c5ae823de063ff81f
froilan <froilanaquino1@gmail.com>
2023-12-07 02:23:53 +0800

    save point #3

--- app/src/main/java/com/example/movies_ticketing_aquino_villaester_edp/MainActivity.java
+++ app/src/main/java/com/example/movies_ticketing_aquino_villaester_edp/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.movies_ticketing_aquino_villaester_edp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R)
+            getWindow().setDecorFitsSystemWindows(false);
+        else {
+            getWindow().setFlags(
+                    WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                    WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+            );
+        }
     }
 }
