20103b37a04588081124276141d8014d08f5f535
froilan <froilanaquino1@gmail.com>
2023-12-10 18:08:45 +0800

    added confirmation dialog

--- app/src/main/java/com/example/movies_ticketing_aquino_villaester_edp/FormActivity.java
+++ app/src/main/java/com/example/movies_ticketing_aquino_villaester_edp/FormActivity.java
@@ -1,14 +1,15 @@
 package com.example.movies_ticketing_aquino_villaester_edp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import java.util.Arrays;
 
 public class FormActivity extends AppCompatActivity {
 
@@ -32,6 +33,18 @@ public class FormActivity extends AppCompatActivity {
         seatsEditText.setText(seat);
 
         buyButton.setOnClickListener(e -> {
+            AlertDialog.Builder confirmationDialog = new AlertDialog.Builder(FormActivity.this);
+            confirmationDialog.setMessage("Make sure the information are all correct.")
+                    .setNegativeButton("LMAO, i did something bad.", (dialog, which) -> {
+                        Toast.makeText(FormActivity.this, "Okay, please check the information carefully.", Toast.LENGTH_LONG).show();
+                    })
+                    .setPositiveButton("LOL, proceed.", (dialog, which) -> {
+                        Intent goToListIntent = new Intent(FormActivity.this, MovieLists.class);
+                        Toast.makeText(FormActivity.this, "Thanks for booking XD.", Toast.LENGTH_LONG).show();
+                        Toast.makeText(FormActivity.this, "We're going back to Movie List after a few seconds.", Toast.LENGTH_LONG).show();
+                        new Handler().postDelayed(() -> startActivity(goToListIntent), 5000);
+                    }).show();
+
             String movieName = movieNameEditText.getText().toString();
             String numTickets = numTicketsEditText.getText().toString();
 
