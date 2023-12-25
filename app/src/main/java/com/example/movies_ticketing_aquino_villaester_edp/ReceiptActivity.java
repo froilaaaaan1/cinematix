a181b9b30b7d72831ca191ea6a351d2ab9f77b1b
froilaaaaan1 <froilanaquino1@gmail.com>
2023-12-25 21:26:43 +0800

    added notification (push) notification of successful booking

--- app/src/main/java/com/example/movies_ticketing_aquino_villaester_edp/ReceiptActivity.java
+++ app/src/main/java/com/example/movies_ticketing_aquino_villaester_edp/ReceiptActivity.java
@@ -1,14 +1,23 @@
 package com.example.movies_ticketing_aquino_villaester_edp;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
 
 public class ReceiptActivity extends AppCompatActivity {
 
@@ -28,6 +37,47 @@ public class ReceiptActivity extends AppCompatActivity {
         Intent receiver = getIntent();
         int seats_count = receiver.getIntExtra("seat_count", 0);
         LinearLayout container = findViewById(R.id.container);
+        String CHANNEL_ID = "";
+        String titleSuccess = "Success!";
+        String contentSuccess = "You have successfully purchased your tickets.";
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Others";
+            String description = "Uncategorized notifications";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            CHANNEL_ID = "main channel";
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+
+            // Register the channel with the system
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.logo_icon)
+                .setContentTitle(titleSuccess)
+                .setContentText(contentSuccess)
+                .setPriority(NotificationCompat.PRIORITY_MAX);
+
+        Handler handler = new Handler();
+        handler.postDelayed(() -> {
+
+            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+            // notificationId is a unique int for each notification that you must define.
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            int notificationId = 1;
+            notificationManager.notify(notificationId, builder.build());
+        }, 5000);
 
 
         for (int i = 0; i < seats_count; i++) {
